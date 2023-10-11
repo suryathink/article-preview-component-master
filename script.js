d227db61c668b57b59725d3556c86894332a31c3
Surya Prakash Singh <suryaprakashsingh110@gmail.com>
2023-10-11 22:39:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,14 @@
 const showShare = document.getElementById("showShare");
 const hideButton = document.getElementById("share-box");
+
 showShare.addEventListener("click", () => {
   if (window.innerWidth >= 900) {
-    showShare.classList.toggle("share-svg-logo-normal");
     showShare.classList.toggle("share-svg-logo-onClick");
+
     hideButton.classList.toggle("hidden");
   } else if (window.innerWidth < 900) {
-    // hideButton.classList.toggle("hidden-mobile");
-    // hideButton.classList.toggle("hidden");
+    hideButton.classList.toggle("hidden");
 
-    showShare.addEventListener("mouseover", function () {
-      hideButton.classList.remove("hidden");
-      hideButton.classList.add("hidden-mobile");
-    });
-
-    showShare.addEventListener("mouseout", function () {
-      hideButton.classList.remove("hidden-mobile");
-      hideButton.classList.add("hidden");
-    });
+    hideButton.classList.toggle("hidden-mobile");
   }
 });
